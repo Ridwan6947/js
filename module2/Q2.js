345ae28b915efac73984f101a1186ec0c3b3f57b
Ridwa <ridwanahamed47@gmail.com>
2024-06-12 18:19:16 +0530

    Module 2 practice questions

---
+++ module2/Q2.js
@@ -0,0 +1,6 @@
+const arr = [0, 2, 4, 6, 8, 10, 12, 14, 16];
+
+const twiceArray = arr.map(num => num * 2);
+const halfArray = arr.map(num => num / 2);
+console.log(twiceArray);
+console.log(halfArray);
