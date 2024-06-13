5544e48e50643d548e6a1680cca7dc566f457981
Ridwa <ridwanahamed47@gmail.com>
2024-06-13 11:10:07 +0530

    module 5 question completed

---
+++ module5/script.js
@@ -0,0 +1,16 @@
+
+function captureEventHandler(event) {
+    console.log('Capture phase event listener triggered.');
+    alert('Capture phase event listener triggered.');
+}
+
+function bubbleEventHandler(event) {
+    console.log('Bubbling phase event listener triggered.');
+    alert('Bubbling phase event listener triggered.');
+}
+
+const container = document.getElementById('container');
+
+container.addEventListener('click', captureEventHandler, true);
+
+container.addEventListener('click', bubbleEventHandler, false);
