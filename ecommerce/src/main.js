ac465b767729b987e0538883dae9891329ff6423
Ridwa <ridwanahamed47@gmail.com>
2024-06-22 12:07:52 +0530

    implemented internationalization for different langauges

--- ecommerce/src/main.js
+++ ecommerce/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from  './store/index.js'
+import i18n from  './i18n.js'
 
-createApp(App).use(store).mount('#app');
+createApp(App).use(store).use(i18n).mount('#app');
